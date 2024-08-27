d7f37e4a9e692be15ebb526427538d31905a049f
Ariel Calipay <83052833+arielcalipay20@users.noreply.github.com>
2024-08-27 11:11:22 +0800

    Welcome Screen
    Welcome Screen

--- app/page2.tsx
+++ app/page2.tsx
@@ -1,80 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'expo-router';
-import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Image, Dimensions } from 'react-native';
 import Loader from '../components/Loader'; // A
 import Button from '../components/Button';
-import TopNav from '../components/TopNav';
+import CustomHeader from '@/components/CustomHeader';
 
 import pipesImage from '../assets/images/pipes.png'
 
-const Index: React.FC = () => {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
+const { width, height } = Dimensions.get('window');
 
-  useEffect(() => {
-    // Simulate a data fetch or any async operation
-    const fetchData = async () => {
-      try {
-        // Simulate a delay
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-      } catch (error) {
-        // Handle errors here
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
+const Page2: React.FC = () => {
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TopNav />
-      <Text style={styles.text}>SOME BIG TEXT</Text>
-      {/* <Button
-        title="Go to Tabs Index"
-        onPress={() => router.push('/menu')}
-      /> */}
-      <Text style={styles.text2}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus officia in, rerum ipsam a rem.
-      </Text>
-      <Image style={styles.image}
-        source={pipesImage}
-      />
+    <SafeAreaView style={styles.safeArea}>
+      <CustomHeader />
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>SOME BIG TEXT</Text>
+          <Text style={styles.text2}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus officia in, rerum ipsam a rem.
+          </Text>
+        </View>
 
-      <Button name='Get Started' />
-    </SafeAreaView >
+        <Image style={styles.image} source={pipesImage} />
+        <Button name='Get Started' route='/menu' />
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default Index;
-
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginHorizontal: '8%',
+    paddingHorizontal: '5%',
+    paddingVertical: '10%',
+  },
+  textContainer: {
+    marginHorizontal: width * 0.18,
   },
   text: {
-    fontSize: 32,
+    fontSize: width * 0.08, // Responsive font size
     fontFamily: 'Rubik',
-    marginBottom: 60,
-    fontWeight: 'bold',
-    color: 'black'
+    marginBottom: height * 0.03, // Responsive margin
+    fontWeight: '900',
+    color: 'black',
+    textAlign: 'center',
   },
   image: {
-    marginBottom: 20
+    marginBottom: height * 0.05, // Responsive margin
   },
   text2: {
-    fontSize: 16,
+    fontSize: width * 0.04, // Responsive font size
     fontFamily: 'Rubik',
     textAlign: 'center',
-    marginBottom: 50,
-    color: 'black'
+    marginBottom: height * 0.04, // Responsive margin
+    color: 'black',
   },
 });
+
+export default Page2;
+
+
+
