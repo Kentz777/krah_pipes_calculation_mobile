515172576d1179c9718c434a63f26f59382ff790
Ariel Calipay <83052833+arielcalipay20@users.noreply.github.com>
2024-08-29 13:31:26 +0800

    Fixes
    Fixes

--- app/menu/index.tsx
+++ app/menu/index.tsx
@@ -7,7 +7,6 @@ import StormwaterIcon from '@/components/StormwaterIcon';
 import PressureIcon from '@/components/PressureIcon';
 import StructuralIcon from '@/components/StructuralIcon';
 import HydraulicIcon from '@/components/HydraulicIcon';
-import { useFonts } from 'expo-font';
 
 const { width, height } = Dimensions.get('window');
 
@@ -19,13 +18,6 @@ const CalculationIcon: React.FC<{ focused: boolean; IconComponent: React.FC<{ co
 };
 
 const Index = () => {
-  const [fontsLoaded] = useFonts({
-    "Rubik": require("../assets/fonts/Rubik.ttf"),
-  })
-
-  if (!fontsLoaded) {
-    return undefined;
-  }
   
   return (
     <View style={styles.container}>
